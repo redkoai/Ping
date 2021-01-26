70a692d22d9c867514232e4a65a8da06a4f1c438
Joey Bodoia <jbodoia21@cmc.edu>
2021-01-26 09:52:35 -0800

    Revert "navbar"
    This reverts commit 1bf63ac49f7b3e7a3766ff0f81d44464ca3c828c.
    
    "accidentally commited to master, reverting and recommitting"

--- App.js
+++ App.js
@@ -1,31 +1,20 @@
-// import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native'
-// import { createStackNavigator } from '@react-navigation/stack'
-import NavBar from './src/navbars/NarBar.js'
-
-// const Stack = createStackNavigator();
-
 
 export default function App() {
-
   return (
-    <NavigationContainer>
-      <View style={styles.container}>
-        <Text>Ping</Text>
-        <StatusBar style="auto" />
-      </View>
-      <NavBar/>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'green',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
