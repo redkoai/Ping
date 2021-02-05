42f59939247e5d1fcc08f5dc530f4697c8924ad3
sepsol <SR-75@live.com>
2021-02-05 14:26:26 -0800

    multiple configurations for implementing fonts

--- App.js
+++ App.js
@@ -2,31 +2,39 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
-import NavBar from './src/navbars/NarBar.js'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import NavBar from './src/navbars/NarBar.js';
+import {
+  useFonts,
+  FiraSans_400Regular,
+  FiraSans_500Medium,
+  FiraSans_600SemiBold,
+  FiraSans_700Bold,
+} from '@expo-google-fonts/fira-sans';
 
 const Stack = createStackNavigator();
 
-import HomeScreenEmpty from './src/screens/home/HomeScreenEmpty'
-import Events from './src/screens/home/Events'
-import Messages from './src/screens/home/Messages'
-
+import SignIn from './src/screens/auth/SignIn';
+import HomeScreenEmpty from './src/screens/home/HomeScreenEmpty';
+import Events from './src/screens/home/Events';
+import Messages from './src/screens/home/Messages';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ FiraSans_600SemiBold });
+  if (!fontsLoaded) return null;
 
   return (
     <NavigationContainer>
       <Stack.Navigator
-      screenOptions={{
-        headerShown: false
-      }}>
-        <Stack.Screen name="HomeScreenEmpty" component={HomeScreenEmpty}/>
-        <Stack.Screen name="Events" component={Events}/>
-        <Stack.Screen name="Messages" component={Messages}/>
-
-
-
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="SignIn" component={SignIn} />
+        <Stack.Screen name="HomeScreenEmpty" component={HomeScreenEmpty} />
+        <Stack.Screen name="Events" component={Events} />
+        <Stack.Screen name="Messages" component={Messages} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/styles/styles.js
+++ src/styles/styles.js
@@ -1,28 +1,96 @@
-import {StyleSheet} from "react-native";
+import { StyleSheet } from 'react-native';
 // import { Dimensions } from 'react-native';
-import {widthPercentageToDP,heightPercentageToDP} from '../../util/scaler'
+import { widthPercentageToDP, heightPercentageToDP } from '../../util/scaler';
 
 const styles = StyleSheet.create({
+  homeEmpty: {
+    flex: 1,
+    resizeMode: 'contain',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    opacity: 1,
+  },
 
-    homeEmpty: {
-        flex: 1,
-        resizeMode:'contain',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width:'100%',
-        opacity: 1
-    },
+  homeEmpty1: {
+    flex: 1,
+    resizeMode: 'contain',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    opacity: 1,
+  },
+});
 
-    homeEmpty1: {
-        flex: 1,
-        resizeMode:'contain',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width:'100%',
-        opacity: 1
-    }
-
-})
-
+export const textStyles = StyleSheet.create({
+  // big
+  bigRegular: {
+    fontFamily: 'FiraSans_400Regular',
+    fontSize: 14,
+  },
+  bigMedium: {
+    fontFamily: 'FiraSans_500Medium',
+    fontSize: 14,
+  },
+  bigSemiBold: {
+    fontFamily: 'FiraSans_600SemiBold',
+    fontSize: 14,
+  },
+  bigBold: {
+    fontFamily: 'FiraSans_700Bold',
+    fontSize: 20,
+  },
+  // normal
+  normalRegular: {
+    fontFamily: 'FiraSans_400Regular',
+    fontSize: 14,
+  },
+  normalMedium: {
+    fontFamily: 'FiraSans_500Medium',
+    fontSize: 14,
+  },
+  normalSemiBold: {
+    fontFamily: 'FiraSans_600SemiBold',
+    fontSize: 14,
+  },
+  normalBold: {
+    fontFamily: 'FiraSans_700Bold',
+    fontSize: 14,
+  },
+  // small
+  smallRegular: {
+    fontFamily: 'FiraSans_400Regular',
+    fontSize: 12,
+  },
+  smallMedium: {
+    fontFamily: 'FiraSans_500Medium',
+    fontSize: 12,
+  },
+  smallSemiBold: {
+    fontFamily: 'FiraSans_600SemiBold',
+    fontSize: 12,
+  },
+  smallBold: {
+    fontFamily: 'FiraSans_700Bold',
+    fontSize: 12,
+  },
+  // tiny
+  tinyRegular: {
+    fontFamily: 'FiraSans_400Regular',
+    fontSize: 8,
+  },
+  tinyMedium: {
+    fontFamily: 'FiraSans_500Medium',
+    fontSize: 8,
+  },
+  tinySemiBold: {
+    fontFamily: 'FiraSans_600SemiBold',
+    fontSize: 8,
+  },
+  tinyBold: {
+    fontFamily: 'FiraSans_700Bold',
+    fontSize: 8,
+  },
+});
 
 export default styles;
