74da8f4ebcf033eb6139c15983e390c41de5208e
sepsol <SR-75@live.com>
2021-02-05 23:46:48 -0800

    improvements to the style of the inputs

--- src/components/CustomTextInput.js
+++ src/components/CustomTextInput.js
@@ -1,8 +1,15 @@
 import { textStyles } from '../styles/styles';
-import { widthPercentageToDP } from '../../util/scaler';
+import { heightPercentageToDP, widthPercentageToDP } from '../../util/scaler';
 
-import React from 'react';
-import { View, Text, TextInput, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 import { Controller } from 'react-hook-form';
 
@@ -29,25 +36,43 @@ function CustomTextInput({
     secureTextEntry,
   },
 }) {
+  const [focus, setFocus] = useState(rules.autoFocus);
+  const [secure, setSecure] = useState(rules.secureTextEntry);
   return (
     <View style={{ width: widthPercentageToDP(90) }}>
       <Text style={textStyles.normalSemiBold}>{input.label}</Text>
       <Controller
         control={control}
         render={({ onChange, onBlur, value }) => (
-          <TextInput
-            style={styles.input}
-            onBlur={onBlur}
-            onChangeText={(value) => onChange(value)}
-            value={value}
-            placeholder={input.placeholder}
-            textContentType={rules.contentType}
-            keyboardType={rules.keyboardType}
-            autoCapitalize={rules.autoCapitalize}
-            autoCorrect={rules.autoCorrect}
-            autoFocus={rules.autoFocus}
-            secureTextEntry={rules.secureTextEntry}
-          />
+          <View style={styles.container}>
+            <TextInput
+              style={[styles.input, focus && styles.inputFocused]}
+              onBlur={onBlur}
+              onChangeText={(value) => onChange(value)}
+              value={value}
+              placeholder={input.placeholder}
+              textContentType={rules.contentType}
+              keyboardType={rules.keyboardType}
+              autoCapitalize={rules.autoCapitalize}
+              autoCorrect={rules.autoCorrect}
+              autoFocus={rules.autoFocus}
+              onFocus={() => setFocus(true)}
+              onBlur={() => setFocus(false)}
+              secureTextEntry={secure}
+            />
+            {rules.secureTextEntry && (
+              <TouchableOpacity
+                style={styles.icon}
+                onPress={() => setSecure(!secure)}
+              >
+                <Ionicons
+                  name={secure ? 'ios-eye' : 'ios-eye-off'}
+                  size={30}
+                  color="#555"
+                />
+              </TouchableOpacity>
+            )}
+          </View>
         )}
         name={input.name}
         rules={input.validation}
@@ -59,17 +84,32 @@ function CustomTextInput({
 }
 
 const styles = StyleSheet.create({
-  input: {
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     marginTop: 10,
     marginBottom: 35,
+  },
+  input: {
+    flex: 1,
     fontFamily: 'FiraSans_400Regular',
     fontSize: 14,
     color: '#555',
-    backgroundColor: '#F7F8FA',
-    borderColor: '#000',
-    height: 50,
     padding: 20,
+    paddingRight: 60,
     borderRadius: 12,
+    borderWidth: 1,
+    borderColor: 'white',
+    backgroundColor: '#F7F8FA',
+  },
+  inputFocused: {
+    backgroundColor: '#F7F8FA',
+    borderColor: '#555',
+  },
+  icon: {
+    position: 'absolute',
+    right: 15,
+    top: heightPercentageToDP(1.4),
   },
 });
 
