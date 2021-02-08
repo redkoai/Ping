0621edd1bfd31ac2a56d786c61b7e3ef0f63371a
sepsol <SR-75@live.com>
2021-02-07 23:30:07 -0800

    adds google client id as an env var to app.json

---
+++ app.config.js
@@ -0,0 +1,15 @@
+// the sole purpose of this file is to add our google's client id to
+// our app.json using environment variables so that we don't give it away
+
+export default ({ config }) => {
+  return {
+    ...config,
+    ios: {
+      config: {
+        googleSignIn: {
+          reservedClientId: process.env.GOOGLE_CLIENT_ID,
+        },
+      },
+    },
+  };
+};
