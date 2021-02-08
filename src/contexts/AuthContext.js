bfc13374e90df4645936e3a19b2c3c4197afb74b
sepsol <SR-75@live.com>
2021-02-08 12:30:01 -0800

    created an AuthContext and added google sso

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import * as GoogleSignIn from 'expo-google-sign-in';
+import EncryptedStorage from 'react-native-encrypted-storage';
+
+const AuthContext = React.createContext();
+
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+  useEffect(googleInitAsync, []);
+
+  const googleInitAsync = async () => {
+    await GoogleSignIn.initAsync({
+      // You may ommit the clientId when the firebase `googleServicesFile` is configured
+      clientId: '<YOUR_IOS_CLIENT_ID>',
+    });
+    _googleSyncUserWithStateAsync();
+  };
+
+  const _googleSyncUserWithStateAsync = async () => {
+    const user = await GoogleSignIn.signInSilentlyAsync();
+    setUser(user);
+  };
+
+  const googleSignOutAsync = async () => {
+    await GoogleSignIn.signOutAsync();
+    setUser(null);
+  };
+
+  const googleSignInAsync = async () => {
+    try {
+      await GoogleSignIn.askForPlayServicesAsync();
+      const { type, user } = await GoogleSignIn.signInAsync();
+      if (type === 'success') {
+        _googleSyncUserWithStateAsync();
+      }
+    } catch ({ message }) {
+      alert('login: Error:' + message);
+    }
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        googleSignInAsync,
+        googleSignOutAsync,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export default AuthContext;
