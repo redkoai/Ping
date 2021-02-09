a79123d2e5a72e75cfeb29fc3272165af50acf5d
sepsol <SR-75@live.com>
2021-02-09 11:45:45 -0800

    added all the config for the web firebase app

--- app.config.js
+++ app.config.js
@@ -8,7 +8,22 @@ export default ({ config }) => {
       ...config.ios,
       config: {
         googleSignIn: {
-          reservedClientId: process.env.IOS_CLIENT_ID,
+          reservedClientId: process.env.IOS_RESERVED_CLIENT_ID,
+        },
+      },
+      web: {
+        ...config.web,
+        config: {
+          firebase: {
+            apiKey: process.env.API_KEY,
+            authDomain: process.env.AUTH_DOMAIN,
+            databaseURL: process.env.DATABASE_URL,
+            projectId: process.env.PROJECT_ID,
+            storageBucket: process.env.STORAGE_BUCKET,
+            messagingSenderId: process.env.MESSAGING_SENDER_ID,
+            appId: process.env.APP_ID,
+            measurementId: process.env.MEASUREMENT_ID,
+          },
         },
       },
     },
