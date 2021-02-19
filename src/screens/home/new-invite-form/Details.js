a7f9161cc939f9c29251d751f9b4c72ab4e0e1d2
sepsol <SR-75@live.com>
2021-02-19 13:19:35 -0800

    removing unnecessary components

--- src/screens/home/new-invite-form/Details.js
+++ src/screens/home/new-invite-form/Details.js
@@ -2,17 +2,9 @@ import React, { useCallback } from 'react';
 import { useFocusEffect } from '@react-navigation/native';
 import {
   Image,
-  ImageBackground,
   View,
-  ScrollView,
-  KeyboardAvoidingView,
-  TouchableOpacity,
-  TouchableWithoutFeedback,
-  Keyboard,
-  Dimensions,
-  SafeAreaView,
-  ActivityIndicator,
 } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import emptyHome from 'ping/assets/homeScreen/bg.png';
 import styles from 'ping/src/styles/styles';
 import { widthPercentageToDP, heightPercentageToDP } from 'ping/util/scaler';
@@ -38,106 +30,101 @@ function Details({ navigation }) {
   };
 
   return (
-    <TouchableWithoutFeedback
-      onPress={() => {
-        Keyboard.dismiss();
-      }}
-    >
-      <View style={{ flex: 1 }}>
-        <ScrollView>
-          <ImageBackground source={emptyHome} style={styles.homeEmpty}>
-            <View
-              style={{
-                flexDirection: 'column',
-                justifyContent: 'center',
-                marginTop: widthPercentageToDP(3),
+    <>
+      <KeyboardAwareScrollView
+        style={{ flex: 1, backgroundColor: 'white' }}
+        contentContainerStyle={{ justifyContent: 'center', alignItems: 'center' }}
+      >
+        <View
+          style={{
+            flexDirection: 'column',
+            justifyContent: 'center',
+            marginTop: widthPercentageToDP(3),
+          }}
+        >
+          <Image
+            source={deprogline}
+            style={{
+              height: heightPercentageToDP('7'),
+              width: widthPercentageToDP('88'),
+              resizeMode: 'contain',
+              marginTop: heightPercentageToDP('-2'),
+            }}
+          />
+          <CustomTextInput
+            control={control}
+            errors={errors}
+            input={{
+              name: 'event',
+              label: 'Name your event',
+              placeholder: 'Halloween party,Jenns birthday dinner,etc.',
+              defaultValue: '',
+            }}
+          />
+          <DateInput
+            control={control}
+            errors={errors}
+            setValue={setValue}
+            input={{ name: 'start-date', label: 'Start Date' }}
+          />
+          <DateInput
+            control={control}
+            errors={errors}
+            setValue={setValue}
+            input={{ name: 'end-date', label: 'End Date' }}
+          />
+          <LocationInput control={control} errors={errors} setValue={setValue} />
+          <CustomTextInput
+            control={control}
+            errors={errors}
+            input={{
+              name: 'hosted',
+              label: 'Hosted by',
+              placeholder: 'Host/organization name',
+              defaultValue: '',
+            }}
+            optional
+          />
+          <Image
+            source={demsg}
+            style={{
+              height: heightPercentageToDP('40'),
+              width: widthPercentageToDP('35'),
+              marginTop: heightPercentageToDP('-20'),
+              resizeMode: 'contain',
+            }}
+          />
+          <View
+            style={{
+              height: heightPercentageToDP(15),
+              width: widthPercentageToDP(35),
+              marginTop: heightPercentageToDP(-15),
+              resizeMode: 'contain',
+            }}
+          >
+            <CustomTextInput
+              control={control}
+              errors={errors}
+              input={{
+                name: 'description',
+                label: 'Description',
+                placeholder: 'Let people know what this event is about at a glance!',
+                defaultValue: '',
               }}
-            >
-              <Image
-                source={deprogline}
-                style={{
-                  height: heightPercentageToDP('7'),
-                  width: widthPercentageToDP('88'),
-                  resizeMode: 'contain',
-                  marginTop: heightPercentageToDP('-2'),
-                }}
-              />
-              <CustomTextInput
-                control={control}
-                errors={errors}
-                input={{
-                  name: 'event',
-                  label: 'Name your event',
-                  placeholder: 'Halloween party,Jenns birthday dinner,etc.',
-                  defaultValue: '',
-                }}
-              />
-              <DateInput
-                control={control}
-                errors={errors}
-                setValue={setValue}
-                input={{ name: 'start-date', label: 'Start Date' }}
-              />
-              <DateInput
-                control={control}
-                errors={errors}
-                setValue={setValue}
-                input={{ name: 'end-date', label: 'End Date' }}
-              />
-              <LocationInput control={control} errors={errors} setValue={setValue} />
-              <CustomTextInput
-                control={control}
-                errors={errors}
-                input={{
-                  name: 'hosted',
-                  label: 'Hosted by',
-                  placeholder: 'Host/organization name',
-                  defaultValue: '',
-                }}
-                optional
-              />
-              <Image
-                source={demsg}
-                style={{
-                  height: heightPercentageToDP('40'),
-                  width: widthPercentageToDP('35'),
-                  marginTop: heightPercentageToDP('-20'),
-                  resizeMode: 'contain',
-                }}
-              />
-              <View
-                style={{
-                  height: heightPercentageToDP(15),
-                  width: widthPercentageToDP(35),
-                  marginTop: heightPercentageToDP(-15),
-                  resizeMode: 'contain',
-                }}
-              >
-                <CustomTextInput
-                  control={control}
-                  errors={errors}
-                  input={{
-                    name: 'description',
-                    label: 'Description',
-                    placeholder: 'Let people know what this event is about at a glance!',
-                    defaultValue: '',
-                  }}
-                  rules={{ multiline: 'true', numberOfLines: 5 }}
-                  optional
-                />
-              </View>
+              rules={{ multiline: 'true', numberOfLines: 5 }}
+              optional
+            />
+          </View>
 
-              <View style={{ alignSelf: 'flex-end' }}>
-                <CustomButton text="next" onPress={handleSubmit(onSubmit)} width={47} isPrimary />
-              </View>
+          <View style={{ alignSelf: 'flex-end' }}>
+            <CustomButton text="next" onPress={handleSubmit(onSubmit)} width={47} isPrimary />
+          </View>
 
-              <Spacer height={2} />
-            </View>
-          </ImageBackground>
-        </ScrollView>
-        <NavBar_invite />
-      </View>
-    </TouchableWithoutFeedback>
+          <Spacer height={2} />
+        </View>
+      </KeyboardAwareScrollView>
+      <NavBar_invite />
+    </>
   );
 }
 export default Details;
