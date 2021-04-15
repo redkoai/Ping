20ec667a7b71e49a158be416691a8ab4cf8fd388
Oladayo Ogundipe <oladayogn@gmail.com>
2021-04-15 09:24:58 -0700

    accepting empty values for rsvp

--- src/components/inputs/ImagePicker.js
+++ src/components/inputs/ImagePicker.js
@@ -1,20 +1,31 @@
-import React, { useState } from 'react';
-import { View, Button, Image, Text, StyleSheet, Alert, TouchableOpacity } from 'react-native';
-import * as ImagePicker from 'expo-image-picker';
-import * as Permissions from 'expo-permissions';
+import React, { useState } from "react";
+import {
+  View,
+  Button,
+  Image,
+  Text,
+  StyleSheet,
+  Alert,
+  TouchableOpacity,
+} from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import * as Permissions from "expo-permissions";
 // import imageupload from "../assets/upload.png"
-import { widthPercentageToDP, heightPercentageToDP } from 'ping/util/scaler';
-import upload from 'ping/assets/createnew/dresscode/upload.png';
-
+import { widthPercentageToDP, heightPercentageToDP } from "ping/util/scaler";
+import upload from "ping/assets/createnew/dresscode/upload.png";
+//import ImagePicker from "react-native-image-crop-picker";
 const ImgPicker = (props) => {
   const [pickedImage, setPickedImage] = useState();
   const verifyPermissions = async () => {
-    const result = await Permissions.askAsync(Permissions.MEDIA_LIBRARY, Permissions.CAMERA);
-    if (result.status !== 'granted') {
+    const result = await Permissions.askAsync(
+      Permissions.MEDIA_LIBRARY,
+      Permissions.CAMERA
+    );
+    if (result.status !== "granted") {
       Alert.alert(
-        'Insufficient permissions!',
-        'You need to grant camera permissions to use this app.',
-        [{ text: 'Okay' }],
+        "Insufficient permissions!",
+        "You need to grant camera permissions to use this app.",
+        [{ text: "Okay" }]
       );
       return false;
     }
@@ -22,17 +33,26 @@ const ImgPicker = (props) => {
   };
 
   const takeImageHandler = async () => {
-    console.log('pressed');
-    const hasPermission = await verifyPermissions();
-    if (!hasPermission) {
-      return;
-    }
-    const image = await ImagePicker.launchCameraAsync({
+    console.log("pressed");
+    // const hasPermission = await verifyPermissions();
+    // if (!hasPermission) {
+    //   return;
+    // }
+    // const image = await ImagePicker.launchCameraAsync({
+    //   allowsEditing: true,
+    //   aspect: [16, 9],
+    //   quality: 1,
+    // });
+    // setPickedImage(image.uri);
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
       allowsEditing: true,
       aspect: [16, 9],
       quality: 1,
-    });
-    setPickedImage(image.uri);
+    })
+      .then()
+      .catch((e) => console.log(e));
+    console.log(result);
   };
 
   return (
@@ -52,11 +72,11 @@ const ImgPicker = (props) => {
         <Image
           source={upload}
           style={{
-            height: heightPercentageToDP('17'),
-            width: widthPercentageToDP('95'),
+            height: heightPercentageToDP("17"),
+            width: widthPercentageToDP("95"),
             marginTop: heightPercentageToDP(2),
-            left: heightPercentageToDP('-4.5'),
-            resizeMode: 'contain',
+            left: heightPercentageToDP("-4.5"),
+            resizeMode: "contain",
           }}
         />
       </TouchableOpacity>
@@ -66,20 +86,20 @@ const ImgPicker = (props) => {
 
 const styles = StyleSheet.create({
   imagePicker: {
-    alignItems: 'center',
+    alignItems: "center",
   },
   imagePreview: {
-    width: '100%',
+    width: "100%",
     height: 200,
     marginBottom: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderColor: '#ccc',
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: "#ccc",
     borderWidth: 1,
   },
   image: {
-    width: '100%',
-    height: '100%',
+    width: "100%",
+    height: "100%",
   },
 });
 
--- src/screens/home/new-invite-form/Dresscode.js
+++ src/screens/home/new-invite-form/Dresscode.js
@@ -1,29 +1,29 @@
-import React, { useState, useContext,useEffect } from 'react';
-import NewInviteContext from 'ping/src/contexts/NewInviteContext';
-import { yupResolver } from '@hookform/resolvers/yup';
-import { DRESSCODE_SCHEMA } from 'ping/src/schema/dresscodeSchema';
-import { Image, StatusBar, View, Text } from 'react-native';
-import { useForm, Controller } from 'react-hook-form';
-import { colors } from 'ping/src/styles/styles';
-import Spacer from 'ping/src/components/Spacer';
-import { widthPercentageToDP, heightPercentageToDP } from 'ping/util/scaler';
-import RadioButton from 'rn-radio-button';
-import deprogline from 'ping/assets/createnew/dresscode/dresscodeprogline.png';
-import ImagePicker from 'ping/src/components/inputs/ImagePicker';
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
-import CustomTextInput from 'ping/src/components/inputs/CustomTextInput';
-import CustomButton from 'ping/src/components/inputs/CustomButton';
-import CustomInputLabel from 'ping/src/components/inputs/CustomInputLabel';
+import React, { useState, useContext, useEffect } from "react";
+import NewInviteContext from "ping/src/contexts/NewInviteContext";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { DRESSCODE_SCHEMA } from "ping/src/schema/dresscodeSchema";
+import { Image, StatusBar, View, Text } from "react-native";
+import { useForm, Controller } from "react-hook-form";
+import { colors } from "ping/src/styles/styles";
+import Spacer from "ping/src/components/Spacer";
+import { widthPercentageToDP, heightPercentageToDP } from "ping/util/scaler";
+import RadioButton from "rn-radio-button";
+import deprogline from "ping/assets/createnew/dresscode/dresscodeprogline.png";
+import ImagePicker  from "ping/src/components/inputs/ImagePicker";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import CustomTextInput from "ping/src/components/inputs/CustomTextInput";
+import CustomButton from "ping/src/components/inputs/CustomButton";
+import CustomInputLabel from "ping/src/components/inputs/CustomInputLabel";
 
 function Dresscode({ navigation }) {
   const { formData, updateFormData } = useContext(NewInviteContext);
 
-  const { control, errors, setValue,reset, handleSubmit } = useForm({
+  const { control, errors, setValue, reset, handleSubmit } = useForm({
     resolver: yupResolver(DRESSCODE_SCHEMA),
   });
   const onSubmit = (data) => {
     updateFormData(data);
-    navigation.navigate('FAQ');
+    navigation.navigate("FAQ");
     //reset();
     //console.log("After Submit Dresscode---", formData)
   };
@@ -33,37 +33,37 @@ function Dresscode({ navigation }) {
   // }, [formData]);
 
   const listData = [
-    { label: 'Casual and Comfortable', value: 'casual-and-comfortable' },
-    { label: 'Business casual', value: 'business-casual' },
-    { label: 'Semi-formal', value: 'semi-formal' },
-    { label: 'Formal/Black tie', value: 'formal-black-tie' },
-    { label: 'custom', value: 'custom' },
+    { label: "Casual and Comfortable", value: "casual-and-comfortable" },
+    { label: "Business casual", value: "business-casual" },
+    { label: "Semi-formal", value: "semi-formal" },
+    { label: "Formal/Black tie", value: "formal-black-tie" },
+    { label: "custom", value: "custom" },
   ];
 
   return (
     <KeyboardAwareScrollView
-      style={{ flex: 1, backgroundColor: 'white' }}
-      contentContainerStyle={{ justifyContent: 'center', alignItems: 'center' }}
+      style={{ flex: 1, backgroundColor: "white" }}
+      contentContainerStyle={{ justifyContent: "center", alignItems: "center" }}
     >
       <StatusBar backgroundColor={colors.primary} />
       <View
         style={{
-          flexDirection: 'column',
-          justifyContent: 'center',
+          flexDirection: "column",
+          justifyContent: "center",
           marginTop: widthPercentageToDP(3),
         }}
       >
         <Image
           source={deprogline}
           style={{
-            height: heightPercentageToDP('10'),
-            width: widthPercentageToDP('85'),
-            resizeMode: 'contain',
-            marginTop: heightPercentageToDP('-2.5'),
-            left: heightPercentageToDP('2'),
+            height: heightPercentageToDP("10"),
+            width: widthPercentageToDP("85"),
+            resizeMode: "contain",
+            marginTop: heightPercentageToDP("-2.5"),
+            left: heightPercentageToDP("2"),
           }}
         />
-        <View style={{ position: 'relative', left: 10 }}>
+        <View style={{ position: "relative", left: 10 }}>
           <Controller
             control={control}
             name="radio-buttons"
@@ -73,7 +73,7 @@ function Dresscode({ navigation }) {
                 innerWidth={20}
                 borderWidth={1.5}
                 data={listData}
-                color={'black'}
+                color={"black"}
                 onPress={onChange}
                 wrapperStyle={{ padding: 4 }}
                 horizontal={false}
@@ -83,20 +83,20 @@ function Dresscode({ navigation }) {
         </View>
         <View
           style={{
-            height: heightPercentageToDP('12'),
-            width: widthPercentageToDP('95'),
-            marginTop: heightPercentageToDP('1'),
-            right: heightPercentageToDP('-2'),
-            resizeMode: 'contain',
+            height: heightPercentageToDP("12"),
+            width: widthPercentageToDP("95"),
+            marginTop: heightPercentageToDP("1"),
+            right: heightPercentageToDP("-2"),
+            resizeMode: "contain",
           }}
         >
           <CustomTextInput
             input={{
-              name: 'dresscode',
-              label: '',
+              name: "dresscode",
+              label: "",
               placeholder:
                 "Do you have specific theme or color in mind? Don't make your guests guess!",
-              defaultValue: '',
+              defaultValue: "",
             }}
             control={control}
             rules={{ multiline: true, numberOfLines: 5 }}
@@ -105,11 +105,16 @@ function Dresscode({ navigation }) {
 
         <CustomInputLabel text="Share some Inspo" optional />
         <Spacer height={1} />
-        <ImagePicker />
+         <ImagePicker /> 
 
         <Spacer height={5} />
-        <View style={{ alignSelf: 'flex-end' }}>
-          <CustomButton text="next" onPress={handleSubmit(onSubmit)} narrow primary />
+        <View style={{ alignSelf: "flex-end" }}>
+          <CustomButton
+            text="next"
+            onPress={handleSubmit(onSubmit)}
+            narrow
+            primary
+          />
         </View>
         <Spacer height={2} />
       </View>
